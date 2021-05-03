14037b2333329d4e901e71ea128f87a8234f4a3e
Ryan Evanyo <ryan.evanyo@gmail.com>
2021-05-03 09:44:48 -0600

    Bubble Sort Java

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -7,7 +7,7 @@ public class Main {
     public static void main(String[] args) {
         // TODO Auto-generated method stub
         int[] nums = {1,5,3,2,7,8, 4, 45,123,2,4,78};
-        int[] result = bubbleSort(nums);
+        int[] result = insertionSort(nums);
         System.out.print(Arrays.toString(result));
 
     }
@@ -25,8 +25,35 @@ public class Main {
         }
         return arr;
     }
+
+    public static int[] insertionSort(int[] list) {
+        int i,j, current;
+        for(i = 1; i<list.length-1; i++) {
+            current = list[i];
+            j=i-1;
+            while(j >= 0 && list[j] > current) {
+                list[j] = list[j+1];
+                j--;
+            }
+            current = list[j+1];
+        }
+        return list;
+    }
 }
 
+//for (int i = 1; i < n; ++i) {
+//        int key = arr[i];
+//        int j = i - 1;
+//
+//            /* Move elements of arr[0..i-1], that are
+//               greater than key, to one position ahead
+//               of their current position */
+//        while (j >= 0 && arr[j] > key) {
+//        arr[j + 1] = arr[j];
+//        j = j - 1;
+//        }
+//        arr[j + 1] = key;
+//        }
 
 
 
