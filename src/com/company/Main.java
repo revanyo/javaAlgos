6485aded9574a75401105293be781a47754bf388
Ryan Evanyo <ryan.evanyo@gmail.com>
2021-05-04 13:01:46 -0600

    Bubble Sort Java

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,7 +5,6 @@ import java.util.Arrays;
 public class Main {
 
     public static void main(String[] args) {
-        // TODO Auto-generated method stub
         int[] nums = {1,5,3,2,7,8, 4, 45,123,2,4,78};
         int[] result = insertionSort(nums);
         System.out.print(Arrays.toString(result));
