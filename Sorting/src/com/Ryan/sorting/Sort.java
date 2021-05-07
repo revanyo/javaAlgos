468d8c6b45d0619d49d677578d20cb61573972d3
Ryan Evanyo <ryan.evanyo@gmail.com>
2021-05-07 16:34:06 -0600

    Test

---
+++ Sorting/src/com/Ryan/sorting/Sort.java
@@ -0,0 +1,12 @@
+package com.Ryan.sorting;
+
+import java.lang.*;
+import java.util.Arrays;
+
+
+public class Sort {
+	public static void main(String[] args) {
+		int[] numbers = {1,2,3,4,5};
+		System.out.print(Arrays.toString(numbers));
+	}
+}
---
+++ Sorting/src/module-info.java
@@ -0,0 +1,2 @@
+module com.Ryan.sorting {
+}
